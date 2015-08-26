4b52d6442d062565b634376eed5c263de62be60b
Adam <amcfadden@renewfund.com>
2015-08-26 08:42:59 -0700

    WIP add code pens

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function(){
     $('.project-card').each( function(i){
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-      if( bottom_of_window > bottom_of_object - 100 ){
+      if( bottom_of_window > bottom_of_object - 200 ){
           $(this).fadeTo("slow", 1)
       }
     });
