c3ee94581a48247ac7958d88dbba3aac7bdd12b2
Adam <adam@codetheory.io>
2016-10-06 00:04:18 -0700

    Finish implementation of new header - change skills section to toolbox - update skills - update general description

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 $(function(){
-
   var $navbar = $('#navbar'),
-      y_position = $navbar.offset().top,
-      navbar_height = $navbar.height();
+      y_position = $navbar.offset().top;
 
 
   $(window).scroll( function(){
     var scrollTop = $(this).scrollTop();
 
+    // fade cards in as user scrolls
     $('.project-card').each( function(i){
-
       var bottom_of_object = $(this).offset().top + $(this).outerHeight(),
           bottom_of_window = scrollTop + $(window).height();
 
       if( bottom_of_window > bottom_of_object - 200 ){
-          $(this).fadeTo("slow", 2)
+          $(this).fadeTo("slow", 1)
       }
     });
 
-    if (scrollTop > y_position + navbar_height) {
+    // sticky navbar as user scrolls past header
+    if (scrollTop > y_position) {
       $navbar.addClass("navbar-fixed");
     } else if (scrollTop <= y_position) {
       $navbar.removeClass("navbar-fixed");
@@ -31,41 +30,3 @@ $(function(){
 
   $(".button-collapse").sideNav();
 });
-//
-//
-// var num = 200; //number of pixels before modifying styles
-//
-// $(window).bind('scroll', function () {
-//     if ($(window).scrollTop() > num) {
-//         $('.menu').addClass('fixed');
-//     } else {
-//         $('.menu').removeClass('fixed');
-//     }
-// });
-
-
-
-//
-//
-// (function($) {
-//     "use strict";
-//
-//     var $navbar = $("#navbar"),
-//         y_pos = $navbar.offset().top,
-//         height = $navbar.height();
-//
-//     $(document).scroll(function() {
-//         var scrollTop = $(this).scrollTop();
-//
-//         if (scrollTop > y_pos + height) {
-//             $navbar.addClass("navbar-fixed").animate({
-//                 top: 0
-//             });
-//         } else if (scrollTop <= y_pos) {
-//             $navbar.removeClass("navbar-fixed").clearQueue().animate({
-//                 top: "-48px"
-//             }, 0);
-//         }
-//     });
-//
-// })(jQuery, undefined);
